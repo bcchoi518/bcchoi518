54458390adbdc3ca38b14dfcf3c2047693235ade
bcchoi518 <bcchoi518@gmail.com>
2022-02-04 16:40:29 +0900

    Edit vendingmachine

--- C_projects/VendingMachine/VendingMachine/vendingMachine.c
+++ C_projects/VendingMachine/VendingMachine/vendingMachine.c
@@ -8,12 +8,12 @@ int main(void) {
 	char title[30] = "!!BC Vending Machine!!";
 	char menu[100] = "1. 상품 관리\n2. 잔액 관리\n3. 상품 판매\n4. 프로그램 종료";
 	char arrProductName[MAXROW][MAXCOL][100] = { "", };
-	char arrtemp[100] = "";
+	char arrChartemp[100] = "";
 	char balanceReturn = ' ';
 	char choiceTemp = ' ';
 	int arrProductStock[MAXROW][MAXCOL] = { 0, };
 	int arrProductPrice[MAXROW][MAXCOL] = { 0, };
-	int arrCashStock[5] = { 10,10,10,10,10 };
+	int arrCashStock[3] = { 0,0,0 };
 	int menuSelection = 0;
 	int subMenuSelection = 0;
 	int productNum = 0;
@@ -24,20 +24,19 @@ int main(void) {
 	int inputMoney = 0;
 	int balance = 0;
 	int secondOder = 0;
+	int totalCashStock = 0;
 	int cashStockCheck = 0;
 	int cash1000 = 0;
 	int cash500 = 0;
 	int cash100 = 0;
-	int cash50 = 0;
-	int cash10 = 0;
-	int isDup = 0;
-	
+	int saleStatus = 0;
+
 	while (menuSelection != 4) {
 		printf("%s\n", menu);
 		printf("항목 선택 : ");
 		scanf_s("%d", &menuSelection);
 		subMenuSelection = 0;
-		
+
 
 		switch (menuSelection) {
 		case 1:// 상품 관리
@@ -86,8 +85,8 @@ int main(void) {
 					productCol = (productNum - 1) % 8;
 
 					printf("변경할 상품명을 입력하세요 : ");
-					scanf_s("%s", arrtemp, sizeof(arrtemp));
-					strcpy_s(arrProductName[productRow][productCol], sizeof(arrProductName[productRow][productCol]), arrtemp);
+					scanf_s("%s", arrChartemp, sizeof(arrChartemp));
+					strcpy_s(arrProductName[productRow][productCol], sizeof(arrProductName[productRow][productCol]), arrChartemp);
 					printf("가격을 입력하세요 : ");
 					scanf_s("%d", arrProductPrice[productRow] + productCol);
 					printf("재고를 입력하세요 : ");
@@ -142,9 +141,9 @@ int main(void) {
 				printf("1000원 : %d장\n", arrCashStock[0]);
 				printf(" 500원 : %d개\n", arrCashStock[1]);
 				printf(" 100원 : %d개\n", arrCashStock[2]);
-				printf("  50원 : %d개\n", arrCashStock[3]);
-				printf("  10원 : %d개\n", arrCashStock[4]);
-				printf("==================\n");
+				totalCashStock = (arrCashStock[0] * 1000) + (arrCashStock[1] * 500) + (arrCashStock[2] * 100);
+				printf("  합계 : %d원\n", totalCashStock);
+				printf("==================\n\n");
 
 				printf("1. 잔액 재고 변경\n2. 나가기\n");
 				printf("항목 선택 : ");
@@ -166,14 +165,6 @@ int main(void) {
 					else if (monetaryUnit == 100) {
 						printf("변경할 재고의 수량을 입력하시오 : ");
 						scanf_s("%d", arrCashStock + 2);
-					}
-					else if (monetaryUnit == 50) {
-						printf("변경할 재고의 수량을 입력하시오 : ");
-						scanf_s("%d", arrCashStock + 3);
-					}
-					else if (monetaryUnit == 10) {
-						printf("변경할 재고의 수량을 입력하시오 : ");
-						scanf_s("%d", arrCashStock + 4);
 					}
 					else {
 						printf("Error : 잘못 입력하셨습니다.\n화폐 단위를 확인 후 다시 입력하십시오.\n");
@@ -200,6 +191,35 @@ int main(void) {
 			switch (subMenuSelection) {
 			case 1://판매 시작
 				while (subMenuSelection != 2) {
+					cashStockCheck = 0;
+					for (int i = 0; i < 3; i++) {
+						if (totalCashStock == 0) {
+							cashStockCheck = 1;
+							break;
+						}
+						else {
+							saleStatus = 1;
+						}
+					}
+					if (!cashStockCheck && secondOder == 0) {
+						printf("투입할 금액을 입력하여 주십시오(투입 최소 단위 100원) : ");//지폐단위별로 입금받기
+						scanf_s("%d", &inputMoney);
+						if (inputMoney >= 100 && inputMoney % 100 == 0) {
+							balance = inputMoney;
+							cash1000 = balance / 1000;
+							arrCashStock[0] += cash1000;
+							balance %= 1000;
+							cash500 = balance / 500;
+							arrCashStock[1] += cash500;
+							balance %= 500;
+							cash100 = balance / 100;
+							arrCashStock[2] += cash100;
+						}
+						else {
+							printf("투입 최소 단위는 100원 입니다. (반환 금액 : %d)", inputMoney);
+							break;
+						}
+					}
 					printf("\n========================================%s========================================\n\n", title);
 					productCnt = 0;
 					for (int i = MAXROW - 1; i >= 0; i--) {
@@ -217,47 +237,52 @@ int main(void) {
 						}
 						printf("\n");
 						for (int k = MAXCOL - 1; k >= 0; k--) {
-							printf("%9d원  ", arrProductPrice[(MAXROW - 1) - i][(MAXCOL - 1) - k]);
+							if (arrProductPrice[(MAXROW - 1) - i][(MAXCOL - 1) - k] > inputMoney) {
+								arrProductPrice[(MAXROW - 1) - i][(MAXCOL - 1) - k] = 0;
+								printf("%9d원  ", arrProductPrice[(MAXROW - 1) - i][(MAXCOL - 1) - k]);
+							}
+							else {
+								printf("%9d원  ", arrProductPrice[(MAXROW - 1) - i][(MAXCOL - 1) - k]);
+							}
 							if (k % MAXCOL == 0) { break; }
 						}
 						printf("\n");
 					}
 					printf("======================================================================================================\n");
+					printf("%-15s", saleStatus == 1 ? "판매중" : "판매불가");
+					printf("현재 잔액 : %-15d\t\t", inputMoney);
 					printf("%15s", arrCashStock[0] > 0 ? " " : "1000원권 부족");
 					printf("%15s", arrCashStock[1] > 0 ? " " : "500원권 부족");
 					printf("%15s", arrCashStock[2] > 0 ? " " : "100원권 부족");
-					printf("%15s", arrCashStock[3] > 0 ? " " : "50원권 부족");
-					printf("%15s", arrCashStock[4] > 0 ? " " : "10원권 부족");
 					printf("\n======================================================================================================\n\n");
-
-					cashStockCheck = 0;
-					for (int i = 0; i < 5; i++) {
-						if (arrCashStock[i] == 0) {
-							cashStockCheck = 1;
-							break;
-						}
-					}
+					
 					if (cashStockCheck) {
 						printf("자판기내 잔액이 부족합니다.\n잔액 재고를 확인해 주십시오.\n\n");
 						break;
 					}
-					if (!cashStockCheck && secondOder == 0) {
-						printf("금액을 투입하여 주십시오 : ");
-						scanf_s("%d", &inputMoney);
-					}
+					
 					if (!cashStockCheck || secondOder > 0) {
-						printf("상품번호를 입력하여 주십시오 : ");
+						printf("상품번호를 입력하여 주십시오 : "); 
 						scanf_s("%d", &productNum);
 
 						productRow = (productNum - 1) / 8;
 						productCol = (productNum - 1) % 8;
-
-						printf("선택하신 상품은 %s 이고 ", arrProductName[productRow][productCol], sizeof(arrProductName[productRow][productCol]));
-						printf("가격은 %d원 입니다.\n", arrProductPrice[productRow][productCol]);
-
 						balance = inputMoney - arrProductPrice[productRow][productCol];
+
+						if (!strcmp(arrProductName[productRow][productCol], "품절")) {
+							printf("품절된 상품입니다.\n\n");
+							goto balanceReturnQ;
+						}
+						else if (arrProductPrice[productRow][productCol] == 0) {
+							printf("금액이 부족합니다.\n\n");
+							goto balanceReturnQ;
+						}
+						else {
+							printf("선택하신 상품은 %s 이고 ", arrProductName[productRow][productCol], sizeof(arrProductName[productRow][productCol]));
+							printf("가격은 %d원 입니다.\n\n", arrProductPrice[productRow][productCol]);
+						}
 						if (balance >= 0 && arrProductStock[productRow][productCol] > 0) {
-							printf("투입된 금액은 %d원 이고 잔액은 %d원입니다.\n", inputMoney, balance);
+							printf("투입된 금액은 %d원 이고 잔액은 %d원입니다.\n\n", inputMoney, balance);
 							arrProductStock[productRow][productCol] -= 1;
 
 						balanceReturnQ:
@@ -265,21 +290,28 @@ int main(void) {
 							printf("잔액을 반환하시겠습니까? (y/n) : ");
 							scanf_s("%c", &balanceReturn, sizeof(balanceReturn));
 							if (balanceReturn == 'Y' || balanceReturn == 'y') {
-								cash1000 = balance / 1000;
-								balance = balance % 1000;
-								cash500 = balance / 500;
-								balance = balance % 500;
-								cash100 = balance / 100;
-								balance = balance % 100;
-								cash50 = balance / 50;
-								balance = balance % 50;
-								cash10 = balance / 10;
-								printf("반환된 잔액은 1000원권 %d장, 500원권 %d개, 100원권 %d개, 50원권 %d개, 10원권 %d개 입니다.\n\n", cash1000, cash500, cash100, cash50, cash10);
-								arrCashStock[0] -= cash1000;
-								arrCashStock[1] -= cash500;
-								arrCashStock[2] -= cash100;
-								arrCashStock[3] -= cash50;
-								arrCashStock[4] -= cash10;
+								if (balance / 1000 < arrCashStock[0]) {
+									cash1000 = balance / 1000;
+									arrCashStock[0] -= cash1000;
+									balance %= 1000;
+								}
+								if (balance / 500 < arrCashStock[1]) {
+									cash500 = balance / 500;
+									arrCashStock[1] -= cash500;
+									balance %= 500;
+								}
+								if (balance / 100 < arrCashStock[2]) {
+									cash100 = balance / 100;
+									arrCashStock[2] -= cash100;
+									balance %= 100;
+								}
+								if (balance > 0) {
+									printf("자판기내 잔액 잔고가 부족합니다. 고객센터로 문의주시기 바랍니다.\n\n");
+									break;
+								}
+								printf("반환된 잔액은 1000원권 %d장, 500원권 %d개, 100원권 %d개입니다.\n", cash1000, cash500, cash100);
+								printf("이용해 주셔서 감사합니다.\n\n");
+								subMenuSelection = 2;
 								break;
 							}
 							else if (balanceReturn == 'N' || balanceReturn == 'n') {
