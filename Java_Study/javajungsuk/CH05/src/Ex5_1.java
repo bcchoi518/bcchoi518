23c48d54acec34ff4c2d37464dea4fbc3662cb90
bcchoi518 <bcchoi518@gamil.com>
2022-03-07 21:15:35 +0900

    add java study

---
+++ Java_Study/javajungsuk/CH05/src/Ex5_1.java
@@ -0,0 +1,10 @@
+// 하나의 소스파일에는 하나의 클래스만 작성하는 것이 바람직
+// 하나의 소스파일에는 하나의 public class만 허용
+public class Ex5_1 {
+} // 소스파일의 이름은 public class 이름과 일치해야함
+
+class Ex5_1_1 {
+} // public class 가 없다면 class 이름중 아무거나 소스파일의 이름으로 할 수 있음
+
+class Ex5_1_2 {
+}
