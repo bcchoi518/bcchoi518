d728c6e2c21d5bbb21c95a16ddf091ef00de0192
bcchoi518 <bcchoi518@gamil.com>
2022-04-13 23:22:01 +0900

    study python

--- Python_Study/practice.py
+++ Python_Study/practice.py
@@ -455,3 +455,272 @@ print("Quiz")
 # print("커피 당첨자 : {0}".format(winners[1:]))
 # print(" -- 축하합니다 -- ")
 print("---------------------------------------------------")
+
+print("if")
+# weather = input("오늘 날씨는 어때요? ")
+# if weather == "비" or weather == "눈":
+#     print("우산을 챙기세요")
+# elif weather == "미세먼지":
+#     print("마스크를 챙기세요")
+# else:
+#     print("준비물 필요 없어요.")
+
+# temp = int(input("기온은 어때요? "))  # input은 문자열을 입력받으므로 int()로 감싸서 형변환을 함
+# if 30 <= temp:
+#     print("너무 더워요. 나가지 마세요")
+# elif 10 <= temp and temp < 30:
+#     print("괜찮은 날씨에요")
+# elif 0 <= temp < 10:
+#     print("외투를 챙기세요")
+# else:
+#     print("너무 추워요. 나가지 마세요")
+print("---------------------------------------------------")
+
+print("for")
+# for waiting_no in [0, 1, 2, 3, 4]:
+#     print("대기번호 : {0}".format(waiting_no))
+
+# for waiting_no in range(1, 6): # 1, 2, 3, 4, 5
+#     print("대기번호 : {0}".format(waiting_no))
+
+# starbucks = ["아이언맨", "토르", "아이엠 그루트"]
+# for customer in starbucks:
+#     print("{0}, 커피가 준비되었습니다.".format(customer))
+print("---------------------------------------------------")
+
+print("while")
+# customer = "토르"
+# index = 5
+# while index >= 1:
+#     print("{0}, 커피가 준비 되었습니다. {1} 번 남았어요.".format(customer, index))
+#     index -= 1
+#     if index == 0:
+#         print("커피는 폐기처분되었습니다.")
+
+# customer = "아이언맨"
+# index = 1
+# while True:
+#     print("{0}, 커피가 준비되었습니다. 호출 {1} 회".format(customer, index))
+#     index += 1
+
+# customer = "토르"
+# person = "Unknown"
+
+# while person != customer:
+#     print("{0}, 커피가 준비되었습니다.".format(customer))
+#     person = input("이름이 어떻게 되세요? ")
+print("---------------------------------------------------")
+
+print("continue와 break")
+# absent = [2, 5]  # 결석
+# no_book = [7]  # 책을 깜빡했음
+# for student in range(1, 11):
+#     if student in absent:
+#         continue
+#     elif student in no_book:
+#         print("오늘 수업 여기까지. {0}는 교수실로 따라와".format(student))
+#         break
+#     print("{0}, 책을 읽어봐".format(student))
+print("---------------------------------------------------")
+
+print("한 줄 for")
+# 출석번호가 1 2 3 4 앞에 100을 붙이기로 함 -> 101, 102, 103, 104 ...
+# students = [1, 2, 3, 4, 5]
+# print(students)
+# students = [i+100 for i in students]
+# print(students)
+
+# 학생 이름을 길이로 변환
+# students = ["Iron man", "Thor", "I am groot"]
+# students = [len(i) for i in students]
+# print(students)
+
+# 학생 이름을 대문자로 변환
+# students = ["Iron man", "Thor", "I am groot"]
+# students = [i.upper() for i in students]
+# print(students)
+print("---------------------------------------------------")
+
+print("Quiz")
+# 당신은 Cocoa 서비스를 이용하는 택시 기사님입니다.
+# 50명의 승객과 매칭 기회가 있을 때, 총 탑승 승객 수를 구하는 프로그램을 작성하시오.
+
+# 조건1: 승객별 운행 소요 시간은 5분 ~ 50분 사이의 난수로 정해집니다.
+# 조건2: 당신은 소요 시간 5분 ~ 15분 사이의 승객만 매칭해야 합니다.
+
+# (출력문 예제)
+# [O] 1번째 손님 (소요시간 : 15분)
+# [ ] 2번째 손님 (소요시간 : 50분)
+# [O] 3번째 손님 (소요시간 : 5분)
+# ...
+# [ ] 50번째 손님 (소요시간 : 16분)
+
+# 총 탑승 승객 : 2분
+
+# from random import *
+
+# count = 0
+
+# for i in range(1, 51):
+#     useTime = randrange(5, 51)
+#     if 5 <= useTime <= 15:
+#         print("[O] {0}번째 손님 (소요시간 : {1}분)".format(i, useTime))
+#         count += 1
+#     else:
+#         print("[ ] {0}번째 손님 (소요시간 : {1}분)".format(i, useTime))
+
+# print("총 탑승 승객 : {0}분".format(count))
+print("---------------------------------------------------")
+
+print("함수")
+
+# 함수 정의
+# def open_account():
+#     print("새로운 계좌가 생성되었습니다.")
+
+# 함수 호출
+# open_account()
+print("---------------------------------------------------")
+
+print("전달값과 반환값")
+
+
+# def open_account():
+#     print("새로운 계좌가 생성되었습니다.")
+
+
+# def deposit(balance, money):
+#     print("입금이 완료되었습니다. 잔액은 {0} 원입니다.".format(balance + money))
+#     return balance + money
+
+
+# def withdraw(balance, money):  # 출금
+#     if balance >= money:
+#         print("출금이 완료되었습니다. 잔액은 {0} 원입니다.".format(balance - money))
+#         return balance - money
+#     else:
+#         print("출금이 완료되지 않았습니다. 잔액은 {0} 원입니다.".format(balance))
+#         return balance
+
+
+# def withdraw_night(balance, money):  # 저녁에 출금
+#     commission = 100  # 수수료 100원
+#     return commission, balance - money - commission  # 두개의 값을 ,로 구분하여 반환
+
+
+# balance = 0  # 잔액
+# balance = deposit(balance, 1000)
+# # balance = withdraw(balance, 2000)
+# # balance = withdraw(balance, 500)
+# commission, balance = withdraw_night(balance, 500)
+# print("수수료 {0} 원이며, 잔액은 {1} 원입니다.".format(commission, balance))
+print("---------------------------------------------------")
+
+print("기본값")
+
+
+# def profile(name, age, main_lang):
+#     print("이름 : {0}\t나이 : {1}\t주 사용 언어 : {2}".format(name, age, main_lang))
+
+
+# profile("유재석", 20, "파이썬")
+# profile("김태호", 25, "자바")
+
+# 같은 학교, 같은 학년, 같은 반, 같은 수업.
+# def profile(name, age=17, main_lang="파이썬"):
+#     print("이름 : {0}\t나이 : {1}\t주 사용 언어 : {2}".format(name, age, main_lang))
+
+
+# profile("유재석")
+# profile("김태호")
+print("---------------------------------------------------")
+
+print("키워드값")
+
+
+# def profile(name, age, main_lang):
+#     print(name, age, main_lang)
+
+
+# profile(name="유재석", main_lang="파이썬", age=20)
+# profile(main_lang="자바", age=25, name="김태호")
+print("---------------------------------------------------")
+
+print("가변인자")
+
+
+# def profile(name, age, lang1, lang2, lang3, lang4, lang5):
+#     print("이름 : {0}\t나이 : {1}\t".format(name, age), end=" ")
+#     print(lang1, lang2, lang3, lang4, lang5)
+
+
+# profile("유재석", 20, "Python", "Java", "C", "C++", "C#")
+# profile("김태호", 25, "Kotlin", "Swift", "", "", "")
+
+# def profile(name, age, *language): # *변수명 : 가변인자
+#     print("이름 : {0}\t나이 : {1}\t".format(name, age), end=" ")
+#     for lang in language:
+#         print(lang, end=" ")
+#     print()
+
+
+# profile("유재석", 20, "Python", "Java", "C", "C++", "C#", "JavaScript")
+# profile("김태호", 25, "Kotlin", "Swift", "", "", "")
+print("---------------------------------------------------")
+
+print("지역변수와 전역변수")
+# gun = 10
+
+
+# def checkPoint(soldiers):  # 경계근무
+#     # gun = 20 # 지역 변수
+#     global gun  # 전역 공간에 있는 gun 사용
+#     gun = gun - soldiers
+#     print("[함수 내] 남은 총 : {0}".format(gun))
+
+
+# def checkPoint_ret(gun, soldiers):
+#     gun = gun - soldiers
+#     print("[함수 내] 남은 총 : {0}".format(gun))
+#     return gun
+
+
+# print("전체 총 : {0}".format(gun))
+# # checkPoint(2)  # 2명이 경계 근무 나감
+# gun = checkPoint_ret(gun, 2)
+# print("남은 총 : {0}".format(gun))
+print("---------------------------------------------------")
+
+print("Quiz")
+# 표준 체중을 구하는 프로그램을 작성하시오
+
+# * 표준 체중 : 각 개인의 키에 적당한 체중
+
+# (성별에 따른 공식)
+# 남자 : 키(m) X 키(m) X 22
+# 여자 : 키(m) X 키(m) X 21
+
+# 조건1 : 표준 체중은 별도의 함수 내에서 계산
+#       * 함수명 : std_weight
+#       * 전달값 : 키(height), 성별(gender)
+# 조건2 : 표준 체중은 소수점 둘째자리까지 표시
+
+# (출력 예제)
+# 키 175cm 남자의 표준 체중은 67.38kg 입니다.
+
+# height = int(input("키를 입력하세요 : "))
+# gender = input("성별을 입력하세요 : ")
+
+
+# def std_weight(height, gender):
+#     if gender == "남자":
+#         return (height / 100) * (height / 100) * 22
+#     else:
+#         return (height / 100) * (height / 100) * 21
+
+
+# print("키 {0}cm {1}의 표준 체중은 {2}kg 입니다".format(
+#     height, gender, round(std_weight(height, gender), 2)))
+print("---------------------------------------------------")
+
+print("표준입출력")
