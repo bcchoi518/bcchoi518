35614cdbe1ca42c6fae2c4889368a06aca1d777f
bcchoi518 <bcchoi518@gmail.com>
2022-06-03 19:47:39 +0900

    exercise

---
+++ Java_Study/exercise/exc01/src/Ttest.java
@@ -0,0 +1,7 @@
+public class Ttest {
+  public static void main(String[] args) {
+    System.out.println(┌──────────┐);
+    System.out.println(│1.오징어게임│);
+    System.out.println(└──────────┘);
+  }
+}
--- Java_Study/exercise/exc01/src/exc1_1/Ex01.java
+++ Java_Study/exercise/exc01/src/exc1_1/Ex01.java
@@ -1,5 +1,5 @@
 public class Ex01 {
   public static void main(String[] args) {
-
+    System.out.println(┌──────────┐);
   }// end main
 }// end Ex01
