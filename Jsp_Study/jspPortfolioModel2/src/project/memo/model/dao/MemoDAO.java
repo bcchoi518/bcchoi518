fd4fee655f332d8e6177099340165b8dbff74faa
bcchoi518 <bcchoi518@gmail.com>
2022-09-24 10:03:32 +0900

    update memo

--- Jsp_Study/jspPortfolioModel2/src/project/memo/model/dao/MemoDAO.java
+++ Jsp_Study/jspPortfolioModel2/src/project/memo/model/dao/MemoDAO.java
@@ -1,5 +1,38 @@
 package project.memo.model.dao;
 
-public class MemoDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
-}
+import project.common.DB;
+import project.memo.model.dto.MemoDTO;
+
+public class MemoDAO {
+	Connection conn = null;
+	PreparedStatement pstmt = null;
+	ResultSet rs = null;
+	
+	public ArrayList<MemoDTO> getSelectAll() {
+		ArrayList<MemoDTO> memoList = new ArrayList<>();
+		conn = DB.dbConn();
+		try {
+			String sql = "SELECT * FROM memo ORDER BY no DESC";
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				MemoDTO memoDto = new MemoDTO();
+				memoDto.setNo(rs.getInt("no"));
+				memoDto.setWriter(rs.getString("writer"));
+				memoDto.setContent(rs.getString("content"));
+				memoDto.setRegiDate(rs.getDate("regiDate"));
+				memoList.add(memoDto);
+			}//while
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DB.dbConnClose(rs, pstmt, conn);
+		}//try-catch-finally
+		return memoList;
+	}//getSelectAll
+}//MemoDAO
