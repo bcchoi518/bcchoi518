17d42fbfa30108dd96d2d4f52fef24d7d9c102c2
bcchoi518 <bcchoi518@gmail.com>
2022-11-01 14:01:52 +0900

    add: WAS 이해하기

---
+++ Java_Study/was-practice/src/main/java/org/example/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package org.example.calculator;
+
+import org.example.calculator.calculate.*;
+
+import java.util.List;
+
+public class Calculator {
+  private static final List<NewArithmeticOperator> arithmeticOperators = List.of(new AdditionOperator(),
+      new SubtractionOperator(), new MultiplicationOperator(), new DivisionOperator());
+
+  public static int calculator(PositiveNumber operand1, String operator, PositiveNumber operand2) {
+    return arithmeticOperators.stream()
+        .filter(arithmeticOperator -> arithmeticOperator.supports(operator))
+        .map(ArithmeticOperator -> ArithmeticOperator.calculate(operand1, operand2))
+        .findFirst()
+        .orElseThrow(() -> new IllegalArgumentException("올바른 사칙연산이 아닙니다."));
+  }
+
+}
