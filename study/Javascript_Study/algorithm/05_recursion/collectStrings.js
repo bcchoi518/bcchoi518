eddfa5c13de071cc9aa84c15ee355cc942c0843d
bcchoi518 <bcchoi518@gmail.com>
2023-04-17 22:16:40 +0900

    add: javascript problems

---
+++ study/Javascript_Study/algorithm/05_recursion/collectStrings.js
@@ -0,0 +1,35 @@
+const obj = {
+  stuff: "foo",
+  data: {
+    val: {
+      thing: {
+        info: "bar",
+        moreInfo: {
+          evenMoreInfo: {
+            weMadeIt: "baz",
+          },
+        },
+      },
+    },
+  },
+};
+
+function collectStrings(obj) {
+  let stringsArr = [];
+
+  function gatherStrings(o) {
+    for (let key in o) {
+      if (typeof o[key] === "string") {
+        stringsArr.push(o[key]);
+      } else if (typeof o[key] === "object") {
+        return gatherStrings(o[key]);
+      }
+    }
+  }
+
+  gatherStrings(obj);
+
+  return stringsArr;
+}
+
+console.log(collectStrings(obj)); // ["foo", "bar", "baz"])
