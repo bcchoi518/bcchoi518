df3e17bee620c531bcbc3282607033c7fbd15966
bcchoi518 <bcchoi518@gmail.com>
2024-02-27 20:31:43 +0900

    python: attendance update

--- study/Python_Study/attendance/attendance.py
+++ study/Python_Study/attendance/attendance.py
@@ -3,7 +3,6 @@ import sys
 import configparser as parser                     # 외부설정파일을 읽기 위한 import
 import time                                       # 페이지 로딩을 기다리는데에 사용할 time 모듈 import
 from selenium import webdriver                    # selenium의 webdriver를 사용하기 위한 import
-from selenium.webdriver.common.keys import Keys   # selenium으로 키를 조작하기 위한 import
 from selenium.webdriver.common.by import By       # selenium으로 Element를 선택하기 위한 import
 from datetime import datetime                     # 현재 시각을 구하기 위한 import
 
@@ -26,6 +25,21 @@ configs.read('.\data\config.ini')
 id = configs['account']['id']
 pw = configs['account']['pw']
 
+# options 값 읽기
+pcOnOff = configs['options']['pcOnOff']   # 퇴근 후 PC 종료 여부
+holidayStart = configs['options']['holidayStart']   # 휴일 시작일
+holidayEnd = configs['options']['holidayEnd']   # 휴일 종료일
+
+# 오늘 날짜 구하기
+now = datetime.now()
+today = now.date()
+startDate = datetime.strptime(holidayStart, '%Y-%m-%d').date()
+endDate = datetime.strptime(holidayEnd, '%Y-%m-%d').date()
+
+# 설정한 휴일에 포함되면 프로그램 종료 
+if startDate <= today <= endDate :
+  sys.exit()
+
 driver = webdriver.Chrome()   # 크롬 드라이버 실행
 driver.maximize_window()    # 윈도우 창 크기 최대사이즈로 설정
 driver.get('https://gonet.daouoffice.com/login?returnUrl=%2Fapp%2Fehr')   # 원하는 페이지 로딩
@@ -44,7 +58,6 @@ driver.find_element(By.XPATH, '//*[@id="myInfo"]/span/div/div/a[1]').click()
 driver.find_element(By.XPATH, '//*[@id="closeBadge"]/span').click()
 
 # 현재 시각 구하기
-now = datetime.now()
 hr = now.hour
 
 # 현재 시각이 9시 전이면 출근, 18시 이후면 퇴근 버튼 클릭
@@ -52,5 +65,8 @@ if 6 <= hr < 9 :
   driver.find_element(By.XPATH, '//*[@id="workIn"]/span').click()
 elif 17 < hr :
   driver.find_element(By.XPATH, '//*[@id="workOut"]/span').click()
+  if pcOnOff == 'off' :   # 퇴근 후 PC 종료 여부 off 면 즉시 종료
+    time.sleep(2)
+    os.system('shutdown -s -f -t 0')
 
-time.sleep(10)
+time.sleep(2)   # 퇴근 동작 여부를 확인하기 위한 시간
